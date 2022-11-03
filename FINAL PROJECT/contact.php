5809a28f9fd830d4e37ae0f7c474c81db38c3cdd
Heramb <84409645+Heramb4270@users.noreply.github.com>
2022-11-03 19:16:30 +0530

    Frontend

---
+++ FINAL PROJECT/contact.php
@@ -0,0 +1,342 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+-->
+<!doctype html>
+<html lang="zxx">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="keywords"
+    content="Medick Responsive web template, Bootstrap Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+  <title>CONTACT US |KKWAGH</title>
+  <link href="//fonts.googleapis.com/css2?family=Hind+Siliguri:wght@400;500;600;700&display=swap"
+    rel="stylesheet">
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="assets/css/style-starter.css">
+</head>
+
+<body>
+  <!--header-->
+  <div class="header-w3l">
+    <!-- header -->
+    <header id="site-header" class="fixed-top">
+      <div class="container">
+        <nav class="navbar navbar-expand-lg stroke">
+          <a class="navbar-brand" href="index.html">
+            KK<span class="sub-logo">W</span>AGH</span>
+          </a>
+          <!-- if logo is image enable this   
+            <a class="navbar-brand" href="#index.html">
+                <img src="image-path" alt="Your logo" title="Your logo" style="height:35px;" />
+            </a> -->
+          <button class="navbar-toggler  collapsed bg-gradient" type="button" data-toggle="collapse"
+            data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon fa icon-expand fa-bars"></span>
+            <span class="navbar-toggler-icon fa icon-close fa-times"></span>
+            </span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav mx-lg-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.html">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.html">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link"href="services.html">Departments</a>
+              </li>
+             
+              <li class="nav-item active">
+                <a class="nav-link" href="contact.php">Contact</a>
+              </li>
+              <li class="search-bar ml-lg-3 mr-lg-5 mt-lg-0 mt-4">
+                <!--/search-right-->
+                <form class="search position-relative">
+                  <input type="search" class="search__input" name="search" placeholder="Search here.."
+                    onload="equalWidth()" required="">
+                  <span class="fa fa-search search__icon"></span>
+                </form>
+
+                <!--//search-right-->
+              </li>
+
+            </ul>
+
+          </div>
+          <!-- toggle switch for light and dark theme -->
+          <div class="mobile-position">
+            <nav class="navigation">
+              <div class="theme-switch-wrapper">
+                <label class="theme-switch" for="checkbox">
+                  <input type="checkbox" id="checkbox">
+                  <div class="mode-container">
+                    <i class="gg-sun"></i>
+                    <i class="gg-moon"></i>
+                  </div>
+                </label>
+              </div>
+            </nav>
+          </div>
+          <!-- //toggle switch for light and dark theme -->
+        </nav>
+      </div>
+    </header>
+    </div>
+    <!-- //header -->
+    <!--/breadcrumb-bg-->
+    <div class="breadcrumb-bg py-5  w3l-inner-page-breadcrumb">
+      <div class="container pt-lg-5 pt-md-3 p-lg-4 pb-md-3 my-lg-3">
+        <h2 class="title pt-5">Contact Us</h2>
+        <ul class="breadcrumbs-custom-path mt-3 text-center">
+          <li><a href="index.html">Home</a></li>
+          <li class="active"><span class="fa fa-arrow-right mx-2" aria-hidden="true"></span> Contact </li>
+        </ul>
+      </div>
+    </div>
+  <div class="position-relative">
+    <div class="shape overflow-hidden">
+      <svg viewBox="0 0 2880 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0 48H1437.5H2880V0H2160C1442.5 52 720 0 720 0H0V48Z" fill="currentColor"></path>
+      </svg>
+    </div>
+  </div>
+  <!-- banner bottom shape -->
+  <!--/contact-->
+  <section class="w3l-contact-2 py-5" id="contact">
+    <div class="container py-lg-5 py-md-4 py-2">
+      <div class="contact-grids d-grid">
+        <div class="contact-left">
+          <h6 class="title-subhny mb-1">Get in Touch</h6>
+          <h3 class="title-w3l mb-2">Contact Us</h3>
+          <p>We have a dedicated support center for all of your support needs. We usually get back to you within
+            12-24 hours.</p>
+          <div class="cont-details">
+            <div class="cont-top margin-up">
+              <div class="cont-left text-center">
+                <span class="fa fa-map-marker"></span>
+              </div>
+              <div class="cont-right">
+                <h6>Our Address</h6>
+                <p style="color:black"><b> K. K. Wagh Institute of Engineering Education & Research</b>
+              
+                  Hirabai Haridas Vidyanagari, Amrutdham, Panchavati, Nashik – 422009</p>
+              </div>
+            </div>
+            <div class="cont-top margin-up">
+              <div class="cont-left text-center">
+                <span class="fa fa-phone"></span>
+              </div>
+              <div class="cont-right">
+                <h6>Call Us</h6>
+                <p><a href="tel:+91 253 2512876">+91 253 2512876</a></p>
+                <p><a href="tel:+91 253 2512867">+91 253 2512867</a></p>
+                <p><a href="tel:+91 253 2511962">FAX NO:+91 253 2511962</a></p>
+              </div>
+            </div>
+            <div class="cont-top margin-up">
+              <div class="cont-left text-center">
+                <span class="fa fa-envelope-o"></span>
+              </div>
+              <div class="cont-right">
+                <h6>Email Us</h6>
+                <p><a href="mailto:kkwieer@kkwagh.edu.in" class="mail">kkwieer@kkwagh.edu.in</a></p>
+                <p><a href="mailto:kkwe_office@dataone.in" class="mail">kkwe_office@dataone.in</a></p>
+                <p><a href="mailto:kkwieer@gmail.com" class="mail"></a>kkwieer@gmail.com</p>
+              </div>
+            </div>
+            <div class="cont-top margin-up">
+              <div class="cont-left text-center">
+                <span class="fa fa-life-ring"></span>
+              </div>
+              <div class="cont-right">
+                <h6>WEBSITE</h6>
+                <p><a href="https://engg.kkwagh.edu.in/" class="mail">https://engg.kkwagh.edu.in/</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="contact-right">
+          <form  method="post" class="signin-form">
+            <div class="input-grids">
+              <input type="text" name="name" id="w3lName" placeholder="Your Name*" class="contact-input"
+                required="" />
+              <input type="email" name="email" id="w3lSender" placeholder="Your Email*" class="contact-input"
+                required="" />
+              <input type="text" name="subject" id="w3lSubect" placeholder="Subject*" class="contact-input"
+                required="" />
+              <input type="text" name="url" id="w3lWebsite" placeholder="Website URL*" class="contact-input"
+                required="" />
+            </div>
+            <div class="form-input">
+              <textarea name="message" id="w3lMessage" placeholder="Type your message here*" required=""></textarea>
+            </div>
+            <div class="w3l-submit text-lg-right">
+            <button class="btn btn-style btn-primary" name="btn_submit">Send Message</button>
+            </div>
+          </form>
+        </div>
+      </div>
+  </section>
+  <div class="map-iframe">
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3867250.9286021274!2d76.77514344999999!3d18.815426549999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bddeafc3af36477%3A0xc4b9ae83c3a734aa!2sRangubai%20Junnare%20High%20School!5e0!3m2!1sen!2sin!4v1659935391813!5m2!1sen!2sin" width="1500" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+   </div>
+  <section class="w3l-footer-29-main">
+    <div class="footer-29 py-5">
+      <div class="container py-lg-4">
+        <div class="row footer-top-29">
+          <div class="col-lg-4 col-md-6 col-sm-7 footer-list-29 footer-1 pr-lg-5">
+            <div class="footer-logo mb-3">
+              <a class="navbar-brand" href="index.html">Medick</a>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur ipsum elit. Lorem ipsum dolor sit amet elit.</p>
+            <ul class="mt-3">
+              <li><a href="tel:+(21) 255 999 8888"><span class="fa fa-phone"></span> +(21) 255 999 8888</a></li>
+              <li><a href="mailto:medick@mail.com" class="mail"><span class="fa fa-envelope-open-o"></span>
+                  medick@mail.com</a></li>
+            </ul>
+          </div>
+          <div class="col-lg-2 col-md-6 col-sm-5 col-6 footer-list-29 footer-2 mt-sm-0 mt-5">
+
+            <ul>
+              <h6 class="footer-title-29">History</h6>
+              <li><a href="about.html">About Us</a></li>
+              <li><a href="blog.html"> Blog Posts</a></li>
+              <li><a href="services.html">Departments</a></li>
+              <li><a href="#careers"> Careers</a></li>
+              <li><a href="about.html">Team</a></li>
+              <li><a href="contact.html">Contact us</a></li>
+            </ul>
+          </div>
+          <div class="col-lg-2 col-md-6 col-sm-5 col-6 footer-list-29 footer-3 mt-lg-0 mt-5">
+            <h6 class="footer-title-29">Useful Links</h6>
+            <ul>
+              <li><a href="#terms">Terms of service</a></li>
+              <li><a href="#privacy"> Privacy policy</a></li>
+              <li><a href="#doc"> Documentation</a></li>
+              <li><a href="#support"> Support</a></li>
+              <li><a href="#components"> Components</a></li>
+              <li><a href="#changelog"> Changelog</a></li>
+            </ul>
+
+          </div>
+          <div class="col-lg-4 col-md-6 col-sm-7 footer-list-29 footer-4 mt-lg-0 mt-5">
+            <h6 class="footer-title-29">Subscribe to our Newsletter </h6>
+            <p>Enter your email and receive the latest news, updates and special offers from us.</p>
+
+            <form action="#" class="subscribe" method="post">
+              <input type="email" name="email" placeholder="Your Email Address" required="">
+              <button class="btn btn-style btn-primary w-100 mt-3">Subscibe</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="w3l-copyright">
+    <div class="container">
+      <div class="row bottom-copies">
+        <p class="col-lg-8 copy-footer-29">Contact us <a
+            href="https://w3layouts.com/" target="_blank">
+            W3Layouts</a></p>
+
+        <div class="col-lg-4 main-social-footer-29">
+          <a href="#facebook" class="facebook"><span class="fa fa-facebook"></span></a>
+          <a href="#twitter" class="twitter"><span class="fa fa-twitter"></span></a>
+          <a href="#instagram" class="instagram"><span class="fa fa-instagram"></span></a>
+          <a href="#linkedin" class="linkedin"><span class="fa fa-linkedin"></span></a>
+        </div>
+
+      </div>
+    </div>
+
+    <!-- move top -->
+    <button onclick="topFunction()" id="movetop" title="Go to top">
+      &#10548;
+    </button>
+    <script>
+      window.onscroll = function () {
+        scrollFunction()
+      };
+
+      function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          document.getElementById("movetop").style.display = "block";
+        } else {
+          document.getElementById("movetop").style.display = "none";
+        }
+      }
+
+      function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+      }
+    </script>
+  </section>
+  <script src="assets/js/jquery-3.3.1.min.js"></script>
+  <script src="assets/js/theme-change.js"></script>
+  <script>
+    $(function () {
+      $('.navbar-toggler').click(function () {
+        $('body').toggleClass('noscroll');
+      })
+    });
+  </script>
+  <script>
+    $(window).on("scroll", function () {
+      var scroll = $(window).scrollTop();
+
+      if (scroll >= 80) {
+        $("#site-header").addClass("nav-fixed");
+      } else {
+        $("#site-header").removeClass("nav-fixed");
+      }
+    });
+
+    $(".navbar-toggler").on("click", function () {
+      $("header").toggleClass("active");
+    });
+    $(document).on("ready", function () {
+      if ($(window).width() > 991) {
+        $("header").removeClass("active");
+      }
+      $(window).on("resize", function () {
+        if ($(window).width() > 991) {
+          $("header").removeClass("active");
+        }
+      });
+    });
+  </script>
+  <script src="assets/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
+<?php
+include "config.php";
+
+if (isset($_POST['btn_submit']))
+{
+  extract($_POST);
+
+	$insert = mysqli_query($conn,"insert into contact(name, email, subject, url, message) values('$name','$email','$subject','$url','$message')") or die(mysqli_error($conn));
+  if($insert)
+  {
+    echo "<script>";
+    echo "alert('Data inserted');";
+    echo "</script>";
+  }
+  else
+  {
+    echo "<script>";
+    echo "alert('Data not inserted');";
+    echo "</script>";
+  }
+
+}
+?>
